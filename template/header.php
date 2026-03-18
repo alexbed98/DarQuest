d172e6e2c060a373b97bee4e828b7128dedd123f
AnyaMuenchow <anya.muenchow@gmail.com>
2026-03-18 09:23:47 -0400

    h

--- template/header.php
+++ template/header.php
@@ -1,5 +1,7 @@
 <header>
-         <img class="logo" src="TheBoi.png" alt="Logo">
+    <nav class="nav-bar">
+        <a class=""><img class="logo" src="TheBoi.png" alt="Logo"></a>
+
         <h1 class="titre">Accueil</h1>
         <div class="pages">
             <a>Admin</a>
@@ -10,81 +12,5 @@
         <div>
             <img class="avatar" src="close-up-of-grass-1.jpg">
         </div>
-
-
-    <!-- Bannière-Banner-->
-    <img src="<? ?>/banner.jpg" class="img-fluid" alt="La meilleure boutique informatique !">        
-
-    <!--Bloc Navigation Menu principal-Navigation Block Top Menu-->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Fourth navbar example">
-        <div class="container-fluid">
-            
-            <a class="navbar-brand" href="<? ?>"><img src="<? ?>/boutique-logo.png" class="img-fluid" alt="Accueil de la boutique"></a>
-            
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarsExample04">
-                
-                <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                    <li class="nav-item">
-                        <a class="nav-link <? ?>" aria-current="page" href="<??>"><? ?></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="">À propos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link <??>" href="<??>"><? ?></a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Services</a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="">Services</a></li>
-                            <li><a class="dropdown-item" href="">Location</a></li>
-                            <li><a class="dropdown-item" href="">Achat</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link <??>" href="<??>"><??></a>
-                    </li>
-                    
-                    
-                </ul>
-
-                <!--Connexion/Déconnexion/Administration-->
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-
-                        <? ?>
-                        <a class="nav-link" href="<? ?>"><? ?></a>
-                        <??>
-                        <a class="nav-link <? ?>" href="<??>"><? ?></a>
-                        <? ?>
-
-                    </li>        
-                    
-                    <? ?>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Administration</a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="<??>"><??></a></li>
-                            
-                        </ul>
-                    </li>
-                    <??>
-                </ul>
-                <!--Connexion/Déconnexion/Administration-->
-
-                <form class="ms-2" role="search" action="<??>">
-                    <input name="search" class="form-control" type="search" placeholder="Recherche" aria-label="Search">
-                </form>
-
-                
-
-            </div>
-        </div>
     </nav>
-    <!--Bloc Navigation Menu principal-Navigation Block Top Menu -->
-
 </header>
