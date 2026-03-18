e30bc6b6047f5a1f25e311cfdfe02b7322688a7d
AnyaMuenchow <anya.muenchow@gmail.com>
2026-03-18 09:58:50 -0400

    header

--- template/header.php
+++ template/header.php
@@ -4,10 +4,10 @@
 
         <h1 class="titre">Accueil</h1>
         <div class="pages">
-            <a>Admin</a>
-            <a>Inventaire</a>
-            <a>Énigma</a>
-            <a>Panier</a>
+            <a href="">Admin</a>
+            <a href="">Inventaire</a>
+            <a href="">Énigma</a>
+            <a href="">Panier</a>
         </div>
         <div>
             <img class="avatar" src="close-up-of-grass-1.jpg">
