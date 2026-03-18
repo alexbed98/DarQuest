9fc7ca8e66246e63aa3bafa201ab805d1f92cb59
AnyaMuenchow <anya.muenchow@gmail.com>
2026-03-18 11:07:29 -0400

    1

--- template/listItems.php
+++ template/listItems.php
@@ -1,9 +1,70 @@
+<?php
+
+// include_once '../DAL/Database.php';
+//$items = ItemsDAL::selectAll(Database::getConnexion($dbConfig));
+// $affiche = $_GET['submit'] ?? '';
+
+
+$items = [
+    [
+        "id" => 1,
+        "nom" => "Épée de feu",
+        "categorie" => "arme",
+        "image" => "TheBoi.png",
+        "quantite" => 10,
+        "prix" => 100,
+    ],[
+        "id" => 1,
+        "nom" => "Bouclier de glace",
+        "categorie" => "armure",
+        "image" => "TheBoi.png",
+        "quantite" => 10,
+        "prix" => 100,
+    ],
+];
+
+if(isset($_GET['prix'])) {
+echo "prix";
+}
+
+        // <?php foreach($items as $item) : ?>
+
 <div class="catalogue">
     <div class="options">
         <div style="border: 1px solid black; border-radius: 5px;">
             Nb pièces
         </div>
+        <form action="changeAffichage" method="post">
         <div>
+            <legend>Trier par:</legend>
+            <input type="radio" id="prix" value="prix" />
+            <label for="prix">Prix</label>
+            <div></div>
+            <input type="radio" id="type" value="Type" />
+            <label for="type">Type</label>
+            <div></div>
+            <input type="radio" id="" value="" />
+            <label for=""></label>
+        </div>
+        <div>
+            <legend>Filtrer:</legend>
+            <input type="checkbox" id="arme" />
+            <label for="armes">Armes</label>
+            <div></div>
+            <input type="checkbox" id="armure" />
+            <label for="armure">Armures</label>
+            <div></div>
+            <input type="checkbox" id="" />
+            <label for=""></label>
+            <div></div>
+            <input type="checkbox" id="" />
+            <label for=""></label>
+        </div>
+        <div>
+            <!-- <input type="submit" value="Submit" /> -->
+        </div>
+        </form>
+        <!-- <div>
             <legend>Trier par:</legend>
             <input type="radio" id="prix" />
             <label for="prix">Prix</label>
@@ -27,23 +88,23 @@
             <div></div>
             <input type="checkbox" id="" />
             <label for=""></label>
-        </div>
+        </div> -->
     </div>
 
     <div class="list-item">
-        <?php for ($i = 0; $i < 10; $i++) : ?>
+        <?php foreach($items as $item) : ?>
         <div class="item">
             <div style="border: 2px solid black;">
-                <img src="TheBoi.png" alt="Image de l'article" style="width: 100px; height: 100px;">
+                <img src=<?= PRODUCT_IMG . '/' . $item['image'] ?> alt="Image de l'article" style="width: 100px; height: 100px;">
             </div>
             <div
-                style="border: 2px solid black; display: flex; flex-direction: row; align-items: center; justify-content: space-between; padding: 5px; margin-top: 10px;">
-                <div>Item 1</div>
-                <div>qte</div>
-                <div>prix</div>
+                style="border: 2px solid black; display: flex; flex-direction: row; align-items: center; justify-content: space-between; padding: 5px; margin-top: 10px; width: 300px;">
+                <div class="nom"><?= $item["nom"] ?></div>
+                <div class="quantite"><?= $item["quantite"] ?></div>
+                <div class="prix"><?= $item["prix"] ?></div>
                 <button>Ajouter</button>
             </div>
         </div>
-        <?php endfor; ?>
+        <?php endforeach; ?>
     </div>
 </div>
