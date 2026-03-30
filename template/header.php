ad86227bdae580075a513a91118a997909bbc970
MonMuel <samuelbelanger93@yahoo.com>
2026-03-30 15:04:21 -0400

    Logo logout

--- template/header.php
+++ template/header.php
@@ -13,7 +13,13 @@
             <a class='headerButtons' href="">Énigma</a>
             <a class='headerButtons' href="<?= Page::Panier->url() ?>">Panier</a>
             <a href="<?= Page::Connexion->url() ?>"><img class="avatar" src="/public/img/avatar.jpg" alt="Avatar"></a>
-            <a href="logout.php">logout</a>
+            <a href="logout.php" class="logout-btn" title="Se déconnecter">
+                <svg width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
+                    <polyline points="16 17 21 12 16 7"></polyline>
+                    <line x1="21" y1="12" x2="9" y2="12"></line>
+                </svg>
+            </a>
         </div>
 
     </div>
