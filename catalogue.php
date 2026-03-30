fb7d6581b558bad4e2c5ec16eac610d0f169319a
louis <louisjamesleroux@hotmail.com>
2026-03-30 11:58:00 -0400

    ....

--- catalogue.php
+++ catalogue.php
@@ -1,5 +1,11 @@
 <?php
 
+session_start();
+
+if(isset($_SESSION['email'])) {
+    echo "Bienvenue, " . $_SESSION['email'] . "!";
+}
+
 // les require et les include
 // a decommenter quand on les utilisent
 
