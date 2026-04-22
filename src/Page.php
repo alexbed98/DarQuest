8e1db0b44abc1894954aeccadfa5004e9700d855
alexbed98 <alexbed98@gmail.com>
2026-04-22 09:16:11 -0400

    fix page.php

--- src/Page.php
+++ src/Page.php
@@ -25,7 +25,7 @@ enum Page
             Page::Panier => 'Panier',
             Page::Inventaire => 'Inventaire',
             Page::CreationCompte => 'Création de compte',
-            Page::Admin => 'Administration'
+            Page::Admin => 'Administration',
             Page::Profil => 'Profil',
             Page::Enigme => 'Enigme',
         };
